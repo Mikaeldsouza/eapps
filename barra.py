395f0deb05c1d02e3783be7949de7fde17ab0e05
= <mikaelsouzamail@gmail.com>
2020-08-14 15:32:04 -0300

    Add barra.py

---
+++ barra.py
@@ -0,0 +1,36 @@
+from browser import document
+
+code = document["code"]
+
+code.html = """<div class='bar'><a href='https://eapps.tech/ferramentas/'>Precisa mais alguma ferramenta maravilhosa como esta? Clique Aqui!</a></div>
+
+
+<style type='text/css'>
+    .bar{
+    
+    width:100%;color:white;text-align:center;box-sizing:border-box;padding:10px;color:white;
+
+}
+
+.bar a{
+    color:white;
+    }
+
+@media (min-width:320px){
+
+
+    font-size:50px;
+
+
+}
+
+@media (min-width:1024px){
+
+
+    font-size:18px;
+
+
+}
+</style>
+
+"""
